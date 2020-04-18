08f9ac98fca5780d57a97684e89c0087a57c464f
Bogdan Moroz <bogdan.moroz@observis.fi>
2020-04-18 19:12:46 +0300

    Quiz component with 4 options

---
+++ src/Quiz.svelte
@@ -0,0 +1,19 @@
+<script>
+  let result = "";
+  let correctAnswer = "b";
+  function pickAnswer(answer) {
+    if (answer === correctAnswer) {
+      return (result = "Correct!");
+    }
+
+    result = "OOPS";
+  }
+</script>
+
+<div>
+  <h4>{result}</h4>
+  <button on:click={() => pickAnswer('a')}>Answer A</button>
+  <button on:click={() => pickAnswer('b')}>Answer B</button>
+  <button on:click={() => pickAnswer('c')}>Answer C</button>
+  <button on:click={() => pickAnswer('d')}>Answer D</button>
+</div>
