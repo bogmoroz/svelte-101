089812b7af50fb43c06713a82a230dfdb7f300b4
Bogdan Moroz <bogdan.moroz@observis.fi>
2020-04-18 20:39:43 +0300

    Question component, styling

---
+++ src/Question.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let question;
+  let answers = question.incorrect_answers;
+</script>
+
+<h3>
+  {@html question.question}
+</h3>
+
+{#each answers as answer}
+  <p>
+    {@html answer}
+  </p>
+{/each}
--- src/Quiz.svelte
+++ src/Quiz.svelte
@@ -1,16 +1,7 @@
 <script>
-  let result = "";
-  let correctAnswer = "b";
-  let answers = ["a", "b", "c", "d"];
+  import Question from "./Question.svelte";
+
   let quiz = getQuiz();
-
-  function pickAnswer(answer) {
-    if (answer === correctAnswer) {
-      return (result = "Correct!");
-    }
-
-    result = "OOPS";
-  }
 
   async function getQuiz() {
     const res = await fetch(
@@ -34,25 +25,20 @@
 
 <div>
   <button on:click={handleClick}>Get quiz!</button>
+
   <h4>Quiz</h4>
-  {#if result}
-    <h4>{result}</h4>
-  {:else}
-    <h4>Pick an answer</h4>
-  {/if}
 
   {#await quiz}
     <h4>Loading...</h4>
   {:then data}
-    <h3>{data.results[0].question}</h3>
+    <!-- <h3>{data.results[0].question}</h3> -->
+
+    {#each data.results as question}
+      <Question {question} />
+    {/each}
+
   {:catch error}
     <h4>Failed to load quiz</h4>
   {/await}
-
-  {#each answers as answer}
-    <button on:click={() => pickAnswer(answer)}>
-      Answer {answer.toUpperCase()}
-    </button>
-  {/each}
 
 </div>
