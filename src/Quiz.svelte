a42e00f516ca9bbf697e10cb2b11a0470c5527ef
Bogdan Moroz <bogdan.moroz@observis.fi>
2020-04-18 20:27:20 +0300

    Fetching quiz questions from an API

--- src/Quiz.svelte
+++ src/Quiz.svelte
@@ -2,6 +2,7 @@
   let result = "";
   let correctAnswer = "b";
   let answers = ["a", "b", "c", "d"];
+  let quiz = getQuiz();
 
   function pickAnswer(answer) {
     if (answer === correctAnswer) {
@@ -9,6 +10,19 @@
     }
 
     result = "OOPS";
+  }
+
+  async function getQuiz() {
+    const res = await fetch(
+      "https://opentdb.com/api.php?amount=10&category=18&type=multiple"
+    );
+
+    const quiz = await res.json();
+    return quiz;
+  }
+
+  function handleClick() {
+    quiz = getQuiz();
   }
 </script>
 
@@ -19,12 +33,21 @@
 </style>
 
 <div>
+  <button on:click={handleClick}>Get quiz!</button>
   <h4>Quiz</h4>
   {#if result}
     <h4>{result}</h4>
   {:else}
     <h4>Pick an answer</h4>
   {/if}
+
+  {#await quiz}
+    <h4>Loading...</h4>
+  {:then data}
+    <h3>{data.results[0].question}</h3>
+  {:catch error}
+    <h4>Failed to load quiz</h4>
+  {/await}
 
   {#each answers as answer}
     <button on:click={() => pickAnswer(answer)}>
